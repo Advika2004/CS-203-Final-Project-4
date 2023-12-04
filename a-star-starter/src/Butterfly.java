8f575bfde30d1014418c5920b0e90c8fac829341
nika-zamani <129787022+nika-zamani@users.noreply.github.com>
2023-12-04 09:25:33 -0800

    Revert "nonono"
    This reverts commit b19432d9

--- a-star-starter/src/Butterfly.java
+++ a-star-starter/src/Butterfly.java
@@ -1,159 +1,21 @@
 import processing.core.PImage;
 
-import processing.core.PImage;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 
 public class Butterfly extends Movable {
 
-    public Butterfly(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod) {
+    public Butterfly(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod){
         super(id, position, images, actionPeriod, animationPeriod);
     }
 
-    public boolean moveTo(WorldModel world, Entity target, EventScheduler scheduler) {
-        if (this.getPosition().adjacent(target.getPosition())) {
-            world.removeEntity(scheduler, target);
-            return true;
-        } else {
-            Point nextPos = this.nextPosition(world, target.getPosition());
-
-            if (!this.getPosition().equals(nextPos)) {
-                world.moveEntity(scheduler, this, nextPos);
-            }
-            return false;
-        }
+    @Override
+    public boolean moveTo(WorldModel model, Entity target, EventScheduler scheduler) {
+        return false; //change this
     }
-
-    public Point nextPosition(WorldModel world, Point destPos){
-        Point startPoint =  getPosition();
-        Point endPoint =  destPos;
-        List<Point> newPositions = new AStarPathingStrategy()
-                .computePath(startPoint, endPoint,
-                        p -> world.withinBounds(p) && !world.isOccupied(p),//canPassThrough
-                        (p1,p2) -> p1.adjacent(p2), //withinReach
-                        PathingStrategy.CARDINAL_NEIGHBORS  //potentialNeighbors
-                );
-        if (newPositions.isEmpty()) {
-            return startPoint;
-        }
-        return newPositions.get(0);
-    }
-
-//    @Override
-//    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-//        Optional<Entity> butterflyTarget = world.findNearest(this.getPosition(), new ArrayList<>(List.of(Sapling.class)));
-//
-//        if (butterflyTarget.isPresent()) {
-//            Point tgtPos = butterflyTarget.get().getPosition();
-//
-//            if (this.moveTo(world, butterflyTarget.get(), scheduler)) {
-//
-//                Sapling sapling = new Sapling(Sapling.SAPLING_KEY + "_" + butterflyTarget.get().getId(), tgtPos,
-//                        imageStore.getImageList(Sapling.SAPLING_KEY), Sapling.SAPLING_ACTION_ANIMATION_PERIOD,
-//                        Sapling.SAPLING_ACTION_ANIMATION_PERIOD, 0, Sapling.SAPLING_HEALTH_LIMIT);
-//
-//                sapling.scheduleActions(scheduler, world, imageStore);
-//            }
-//        }
-//
-//        scheduler.scheduleEvent(this, new Activity(this, world, imageStore), this.getActionPeriod());
-//    }
-//}
 
     @Override
     public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-        Optional<Entity> butterflyTarget = world.findNearest(this.getPosition(), new ArrayList<>(List.of(Flower.class)));
-
-        if (butterflyTarget.isPresent()) {
-            Point tgtPos = butterflyTarget.get().getPosition();
-
-            if (this.moveTo(world, butterflyTarget.get(), scheduler)) {
-
-                Flower flower = new Flower(Flower.FLOWER_KEY + "_" + butterflyTarget.get().getId(), tgtPos,
-                        imageStore.getImageList(Flower.FLOWER_KEY), Flower.FLOWER_ACTION_ANIMATION_PERIOD,
-                        Flower.FLOWER_ACTION_ANIMATION_PERIOD, 0, Flower.FLOWER_HEALTH_LIMIT);
-
-                flower.scheduleActions(scheduler, world, imageStore);
-            }
-        }
-
-        scheduler.scheduleEvent(this, new Activity(this, world, imageStore), this.getActionPeriod());
+        int direction = new Random().nextInt(0,4);//change this
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//    @Override
-//    public boolean moveTo(WorldModel model, Entity target, EventScheduler scheduler) {
-//        if (this.getPosition().adjacent(target.getPosition())) {
-//            model.removeEntity(scheduler, target);
-//            return true;
-//        } else {
-//            Point nextPos = this.nextPosition(model, target.getPosition());
-//
-//            if (!this.getPosition().equals(nextPos)) {
-//                model.moveEntity(scheduler, this, nextPos);
-//
-//            return false;
-//        }
-//    }
-//
-//    @Override
-//    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-//        int direction = new Random().nextInt(0, 4);//change this
-//
-//        Optional<Entity> butterflyTarget = world.findNearest(this.getPosition(), new ArrayList<>(List.of(Flower.class)));
-//
-//        if (butterflyTarget.isPresent()) {
-//            Point tgtPos = butterflyTarget.get().getPosition();
-//
-//            if (this.moveTo(world, butterflyTarget.get(), scheduler)) {
-//
-//                Flower flower = new Flower(Flower.FLOWER_KEY + "_" + butterflyTarget.get().getId(), tgtPos,
-//                        imageStore.getImageList(Flower.FLOWER_KEY), Flower.FLOWER_ACTION_ANIMATION_PERIOD,
-//                        Flower.FLOWER_ACTION_ANIMATION_PERIOD, 0, Flower.FLOWER_HEALTH_LIMIT);
-//
-//                world.addEntity(flower);
-//                flower.scheduleActions(scheduler, world, imageStore);
-//            }
-//        }
-//
-//        scheduler.scheduleEvent(this, new Activity(this, world, imageStore), this.getActionPeriod());
-//    }
-//
-//
-//    }
-//
-//        public Point nextPosition (WorldModel world, Point destPos){
-//            Point startPoint = getPosition();
-//            Point endPoint = destPos;
-//            List<Point> newPositions = new AStarPathingStrategy()
-//                    .computePath(startPoint, endPoint,
-//                            p -> world.withinBounds(p) && !world.isOccupied(p),//canPassThrough
-//                            (p1, p2) -> p1.adjacent(p2), //withinReach
-//                            PathingStrategy.CARDINAL_NEIGHBORS  //potentialNeighbors
-//                    );
-//            if (newPositions.isEmpty()) {
-//                return startPoint;
-//            }
-//            return newPositions.get(0);
-//        }
-//    }
--- a-star-starter/src/Gnome.java
+++ a-star-starter/src/Gnome.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Gnome extends Dude {
 
-    public Gnome(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod, int resourceLimit) {
+    public Gnome(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod, int resourceLimit, int i) {
         super(id, position, images, actionPeriod, animationPeriod, resourceLimit);
     }
 
@@ -17,49 +17,34 @@ public class Gnome extends Dude {
         }
     }
 
-    @Override
-    public boolean transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
+    public boolean isNearGarden(WorldModel world) {
+        for (Point point : VirtualWorld.Find8SurroundingTiles(this.getPosition())) {
+            if (world.getBackgroundCell(point).equals("garden")) {
+                return true;
+            }
+        }
         return false;
     }
 
-
-//    public boolean transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
-//        return false;
-//    }
-
-//    public boolean isNearGarden(WorldModel world) {
-//        for (Point point : VirtualWorld.Find8SurroundingTiles(this.getPosition())) {
-//            if (world.getBackgroundCell(point).equals("garden")) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-
-//    @Override
-//    public boolean transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
-//        // Check if the instance is either DudeNotFull or DudeFull and near a garden
-//        if ((this.isNearGarden(world, this.getPosition()))){
-//            System.out.println("it has checked");
-//            Gnome gnome = new Gnome(this.getId(), this.getPosition(), this.getImages(),
-//                    this.getActionPeriod(), this.getAnimationPeriod(), this.getResourceLimit());
-//
-//            world.removeEntity(scheduler, this); // Remove current entity (DudeNotFull/DudeFull)
-//            world.addEntity(gnome); // Add new Gnome entity
-//            gnome.scheduleActions(scheduler, world, imageStore); // Schedule Gnome's actions
-//
-//            return true;
-//        }
-//        return false;
-//    }
-
+    @Override
+    public boolean transform(WorldModel world, EventScheduler scheduler, ImageStore imageStore) {
+        if (this.isNearGarden(world)) {
+            Gnome gnome = new Gnome(this.getId(), this.getPosition(), this.getImages(),
+                    this.getActionPeriod(), this.getAnimationPeriod(), this.getResourceLimit(), 0);
+            world.removeEntity(scheduler, this);
+            world.addEntity(gnome);
+            gnome.scheduleActions(scheduler, world, imageStore);
+            return true;
+        }
+            return false;
+    }
 
     private int flowersPlanted = 0;
 
     @Override
     public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
         if (flowersPlanted < 5) {
-            Optional<Entity> target = world.findNearest(this.getPosition(), new ArrayList<>(Arrays.asList(Tree.class)));
+            Optional<Entity> target = world.findNearest(this.getPosition(), new ArrayList<>(Arrays.asList(Tree.class, Sapling.class)));
             if (target.isPresent() && moveTo(world, target.get(), scheduler)){
                 Point nextToTree = findEmptyAdjacentPosition(world, target.get().getPosition());
                 if(nextToTree != null){
@@ -75,9 +60,9 @@ public class Gnome extends Dude {
     }
 
     private Point findEmptyAdjacentPosition(WorldModel world, Point treePosition) {
-        Point rightPosition = new Point(treePosition.x + 1, treePosition.y);
-        if (!world.isOccupied(rightPosition)) {
-            return rightPosition;
+        Point leftPosition = new Point(treePosition.x - 1, treePosition.y);
+        if (!world.isOccupied(leftPosition)) {
+            return leftPosition;
         }
         return null;
     }
